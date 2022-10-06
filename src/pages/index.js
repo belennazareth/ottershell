648306daa556e476e522153cb0c95416607ed3fa
Belén Nazareth Durán <belennazareth29@gmail.com>
2022-10-07 00:50:07 +0200

    Modificación homepage header

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`¡¡Bienvenido a ${siteConfig.title}!!`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
