7533a8790926145a517d51a8ef7f924b84089c9a
davidrgfoss <40008571+davidrgfoss@users.noreply.github.com>
2022-10-10 13:55:24 +0200

    Update docusaurus.config.js
    Arreglado la opción de editar enlaces

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/belennazareth/ottershell',
+            'https://github.com/belennazareth/ottershell/blob/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
