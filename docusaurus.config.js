d86808f30936b0ed0c6474f04eafbf8f35d88b09
davidrgfoss <40008571+davidrgfoss@users.noreply.github.com>
2022-10-20 00:51:41 +0200

    Añadido opción para editar URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/belennazareth/ottershell',
+            'https://github.com/belennazareth/ottershell/blob/main/',
         },
         blog: {
           showReadingTime: true,
