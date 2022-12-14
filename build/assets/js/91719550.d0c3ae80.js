582d22a688de6cea6f136976a738210685362eaa
Belén Nazareth Durán <belennazareth29@gmail.com>
2022-12-14 13:15:39 +0100

    build

--- build/assets/js/91719550.d0c3ae80.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkotter_shell=self.webpackChunkotter_shell||[]).push([[7679],{9878:e=>{e.exports=JSON.parse('{"title":"Extras","slug":"/category/extras","permalink":"/docs/category/extras","navigation":{"previous":{"title":"plsql_basica","permalink":"/docs/Tasks/plsql_basica"},"next":{"title":"Pr\xf3ximamente...","permalink":"/docs/Extras/proximamente"}}}')}}]);
