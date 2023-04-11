979b10d3fb3c2f9a807227cedc3527e529aa942f
Belén Nazareth Durán <belennazareth29@gmail.com>
2023-04-11 12:39:46 +0200

    ccc

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Contenido propio',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.png').default,
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
         Se trata de una página personal donde la documentación aportada es siempre original.
@@ -14,7 +14,7 @@ const FeatureList = [
   },
   {
     title: 'Trabaja conmigo',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.png').default,
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
         Siempre será agradecida cualquier aportación a este blog, ya sea por correción
@@ -24,7 +24,7 @@ const FeatureList = [
   },
   {
     title: 'Aprendizaje continuo',
-    Svg: require('@site/static/img/undraw_docusaurus_react.png').default,
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
         "El saber no ocupa lugar ni tiene edad"
